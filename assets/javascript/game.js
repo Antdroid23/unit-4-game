e8bb1e87611c2168744e0a6dd98fb631bd8f37af
antdroid23 <princemorrison23@yahoo.com>
2018-12-12 20:44:17 -0500

    update for Alex

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,100 @@
+//Pseudo
+
 //There will be four crystals displayed as buttons on the page.
-
 //The player will be shown a random number at the start of the game.
-
 //When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
-
 //Your game will hide this amount until the player clicks a crystal.
-
-//When they do click one, update the player's score counter.
-
+//When they do click one, update the player's score totalScore.
 //The player wins if their total score matches the random number from the beginning of the game.
-
 //The player loses if their score goes above the random number.
-
 //The game restarts whenever the player wins or loses.
-
 //When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
-
 //The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
 
 
-
-
 //Variables 
-var wins, losses, crys1, crys2, crys3, crys4, randomNumber, totalScore;
+var wins, losses, crysRed, crysBlue, crysYellow, crysGreen, randomNumber, totalScore;
 
 wins = 0;
 losses = 0;
-crys1 = math.floor(math.random() * 10) + 1;
-crys2 = math.floor(math.random() * 10) + 1;
-crys3 = math.floor(math.random() * 10) + 1;
-crys4 = math.floor(math.random() * 10) + 1;
-randomNumber = math.floor(math.random() * 100) + 26;
+crysRed = Math.floor(Math.random() * 10) + 1;
+crysBlue = Math.floor(Math.random() * 10) + 1;
+crysYellow = Math.floor(Math.random() * 10) + 1;
+crysGreen = Math.floor(Math.random() * 10) + 1;
+randomNumber = Math.floor(Math.random() * 100) + 26; 
 totalScore = 0;
+
+
+$("#randomNumber").text(randomNumber);
+$("#losses").text(losses);
+$("#wins").text(wins);
+$("#losses").html(losses);
+$("#wins").html(wins);
+$("#totalScore").text(totalScore);
+
  
 //Functions
 function reset(){
-    crys1 = math.floor(math.random() * 10) + 1;
-    crys2 = math.floor(math.random() * 10) + 1;
-    crys3 = math.floor(math.random() * 10) + 1;
-    crys4 = math.floor(math.random() * 10) + 1;
-    randomNumber = math.floor(math.random() * 100) + 26;
+    crysRed = Math.floor(Math.random() * 10) + 1;
+    crysBlue = Math.floor(Math.random() * 10) + 1;
+    crysYellow = Math.floor(Math.random() * 10) + 1;
+    crysGreen = Math.floor(Math.random() * 10) + 1;
+    randomNumber = Math.floor(Math.random() * 100) + 26;
+    $("#randomNumber").text(randomNumber);
     totalScore = 0;
+    $("#totalScore").text(totalScore);
 }
+
+function win() {
+	alert("You win!");
+	wins ++;
+	$("#wins").text(wins);
+	reset();
+}
+
+function lost() {
+	alert("You lose!");
+	losses ++;
+	$("#losses").text(losses);
+	reset();
+}
+
+function evaluate(){
+    if (totalScore === randomNumber){
+        win()
+    }
+    else (totalScore > randomNumber);{
+        lost()
+    }
+}
+
+
+$("#crysRed").click (function(){
+    console.log(totalScore);
+    totalScore = totalScore + crysRed; 
+    console.log(totalScore);
+    $("#totalScore").text(totalScore);
+
+    evaluate(); 
+  });  
+
+ $("#crysBlue").click (function(){
+    totalScore = totalScore + crysBlue;
+    $("#totalScore").text(totalScore);
+
+    evaluate();   
+  });  
+
+ $("#crysYellow").click (function(){
+    totalScore = totalScore + crysYellow;
+    $("#totalScore").text(totalScore);  
+
+    evaluate();   
+  });  
+
+ $("#crysGreen").click (function(){
+    totalScore = totalScore + crysGreen;
+    $("#totalScore").text(totalScore);
+    
+    evaluate();   
+  });  
